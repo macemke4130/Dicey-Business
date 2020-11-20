3c1cb96ece7f6499684c36544c1810644153f5e4
Lucas Mace <lucasmace4130@gmail.com>
2020-11-20 12:53:53 -0600

    Final Commit

--- app.js
+++ app.js
@@ -6,14 +6,13 @@ class Die {
     constructor() {
         this.div = $('<div></div>');
         this.value = random();
-        this.me = dieCount; // Unique identifier outside of the element id --
     }
 
     generateDie() {
         this.div.attr('id', dieCount);
         this.div.text(this.value);
         this.div.addClass('die d-flex align-items-center justify-content-center m-2');
-        $('#table').append(this.div);
+        $('#table').append(this.div); // #table is a div container for the .dice objects --
         dieCount++;
 
         this.div.click(function () {
@@ -27,34 +26,35 @@ class Die {
     roll() {
         this.value = random();
         this.div.text(this.value);
-        $('#sum-result').text('');
+        $('#sum-result').text(''); // Clears Output --
     }
 
     remove() {
         delete diceSet[this.div.attr('id')]; // Empties diceSet array location --
-        $('#' + this.div.attr('id')).remove();
-        diceSet = diceSet.filter(function (x) {return x != null;}); // Removes empty array locations --
+        $('#' + this.div.attr('id')).remove(); // Removes <div> from HTML --
+        diceSet = diceSet.filter(function (x) { return x != null; }); // Removes empty array locations --
         for (let index = 0; index < diceSet.length; index++) {
-            diceSet[index].id = index; // Renames id of the array object --
+            diceSet[index].id = index; // Renames id of the array Die objects --
             $('.die')[index].id = index; // Renames all of the element ids in the HTML --
         }
-        dieCount--;
-        $('#sum-result').text('');
+        dieCount--; // Decrements dieCount for a Die removal --
+        $('#sum-result').text(''); // Clears Output --
     }
 }
 
 let $generate = $('#generate-die'); // Button in HTML --
 $generate.click(function () {
-    if (dieCount < maxDie) {
-        // Puts new Die object in the diceSet array and calls generateDie() --
+    if (dieCount < maxDie) { // Limits number of dice on screen --
+        // Puts new Die object in the diceSet array and calls generateDie() on that object --
         diceSet[dieCount] = new Die;
         diceSet[dieCount].generateDie();
-        $('#sum-result').text('');
+        $('#sum-result').text(''); // Clears Output --
     }
-    
+
 });
 
-function random() {
+// This random() function has nothing to do with an individual Die so I did not see it neccesary to include it in the class --
+function random() { 
     return Math.floor((Math.random() * 6) + 1);
 }
 
@@ -66,12 +66,18 @@ $sum.click(function () {
         x = parseInt(x); // Converts string values to numbers --
         addItUp = addItUp + x;
     }
-    $('#sum-result').text(`The sum of all dice is ${addItUp}.`)
+    if (dieCount == 0) {
+        $('#sum-result').text('There are no dice to add!');
+    } else {
+        $('#sum-result').text(`The sum of all dice is ${addItUp}.`);
+    }
+
 });
 
 let $roll = $('#roll-dice'); // Roll Dice <button) --
 $roll.click(function () {
-    for (let index = 0; index < diceSet.length; index++) { 
+    if (dieCount == 0) { $('#sum-result').text('There are no dice to roll!'); };
+    for (let index = 0; index < diceSet.length; index++) {
         diceSet[index].roll();
     }
 });
